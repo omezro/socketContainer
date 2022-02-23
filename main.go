e28681d525652c7b865c233eb4d4891843f4adc1
蒋遥 <jiangyao@omegas-Mac.local>
2022-02-23 16:09:07 +0800

    on run

--- main.go
+++ main.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"socketContainer/common"
-	"socketContainer/ws"
+  "encoding/json"
+  "fmt"
+  "net/http"
+  "socketContainer/common"
+  "socketContainer/ws"
 
-	"github.com/gorilla/websocket"
+  "github.com/gorilla/websocket"
   "k8s.io/api/core/v1"
-	"k8s.io/client-go/rest"
-  "k8s.io/client-go/tools/remotecommand"
   "k8s.io/client-go/kubernetes"
   "k8s.io/client-go/kubernetes/scheme"
+  "k8s.io/client-go/rest"
+  "k8s.io/client-go/tools/remotecommand"
 )
 
 var clientset *kubernetes.Clientset
 
 type streamHandler struct {
-  wsConn *ws.WsConnection
+  wsConn      *ws.WsConnection
   resizeEvent chan remotecommand.TerminalSize
 }
 
 type xtermMessage struct {
   MsgType string `json:"type"`
-  Input string `json:"input"`
-  Rows uint16 `json:"rows"`
-  Cols uint16 `json:"cols"`
+  Input   string `json:"input"`
+  Rows    uint16 `json:"rows"`
+  Cols    uint16 `json:"cols"`
 }
 
 func (handler *streamHandler) Next() (size *remotecommand.TerminalSize) {
-  ret := <- handler.resizeEvent
+  ret := <-handler.resizeEvent
   size = &ret
-  return 
+  return
 }
 
 func (handler *streamHandler) Read(p []byte) (size int, err error) {
   var (
-    msg *ws.WsMessage
+    msg      *ws.WsMessage
     xtermMsg xtermMessage
   )
   if msg, err = handler.wsConn.WsRead(); err != nil {
@@ -49,14 +49,14 @@ func (handler *streamHandler) Read(p []byte) (size int, err error) {
   }
   if xtermMsg.MsgType == "resize" {
     handler.resizeEvent <- remotecommand.TerminalSize{
-      Width: xtermMsg.Cols,
+      Width:  xtermMsg.Cols,
       Height: xtermMsg.Rows,
     }
-  }else if xtermMsg.MsgType == "input"{
+  } else if xtermMsg.MsgType == "input" {
     size = len(xtermMsg.Input)
     copy(p, []byte(xtermMsg.Input))
   }
-  return 
+  return
 }
 
 func (handler *streamHandler) Write(p []byte) (size int, err error) {
@@ -71,75 +71,86 @@ func (handler *streamHandler) Write(p []byte) (size int, err error) {
 
 func wsHandler(resp http.ResponseWriter, req *http.Request) {
   var (
-    wsConn *ws.WsConnection
-    restConf *rest.Config
-    sshReq *rest.Request
-    podName string 
-    podNs string 
-    containerName string 
-    executor remotecommand.Executor
-    handler *streamHandler
-    err error 
+    wsConn        *ws.WsConnection
+    restConf      *rest.Config
+    sshReq        *rest.Request
+    podName       string
+    podNs         string
+    containerName string
+    executor      remotecommand.Executor
+    handler       *streamHandler
+    err           error
   )
 
   if err = req.ParseForm(); err != nil {
     return
   }
 
-  podName = "drc-redis-cluster-0-0"
-  podNs = "default"
-  containerName = "redis"
+  podNs = req.Form.Get("podNs")
+  podName = req.Form.Get("podName")
+  containerName = req.Form.Get("containerName")
+
+  if wsConn, err = ws.InitWebsocket(resp, req); err != nil {
+    return
+  }
+
+  podName = "redis-a-zs2f7-0"
+  podNs = "lingfang-8286af39-cab6-4363-b89d-39ce951d2b71"
+  containerName = "redis-a-zs2f7"
 
   if restConf, err = common.GetRestConf(); err != nil {
-    goto END 
+    goto END
   }
 
   sshReq = clientset.
-  CoreV1().
-  RESTClient().
-  Post().
-  Resource("pods").
-  Name(podName).
-  Namespace(podNs).
-  SubResource("exec").
-  VersionedParams(&v1.PodExecOptions{
-    Container: containerName,
-    Command: []string{"sh"},
-    Stdin: true,
-    Stdout: true,
-    Stderr: true,
-    TTY: true,
-  }, scheme.ParameterCodec)
+    CoreV1().
+    RESTClient().
+    Post().
+    Resource("pods").
+    Name(podName).
+    Namespace(podNs).
+    SubResource("exec").
+      VersionedParams(&v1.PodExecOptions{
+        Container: containerName,
+        Command:   []string{"sh"},
+        Stdin:     true,
+        Stdout:    true,
+        Stderr:    true,
+        TTY:       true,
+      }, scheme.ParameterCodec)
 
   if executor, err = remotecommand.NewSPDYExecutor(restConf, "POST", sshReq.URL()); err != nil {
-    goto END 
+    goto END
   }
 
   handler = &streamHandler{
-    wsConn: wsConn,
-    resizeEvent: make(chan remotecommand.TerminalSize)}
-    if err = executor.Stream(remotecommand.StreamOptions{
-      Stdin: handler,
-      Stdout: handler,
-      Stderr: handler,
-      TerminalSizeQueue: handler,
-      Tty: true,
-    }); err != nil {
-      goto END 
-    }
-    return
-END: 
+    wsConn:      wsConn,
+    resizeEvent: make(chan remotecommand.TerminalSize),
+  }
+  if err = executor.Stream(remotecommand.StreamOptions{
+    Stdin:             handler,
+    Stdout:            handler,
+    Stderr:            handler,
+    TerminalSizeQueue: handler,
+    Tty:               true,
+  }); err != nil {
+    goto END
+  }
+  return
+END:
   fmt.Println(err)
   wsConn.WsClose()
 }
 
-
 func main() {
-  var err error 
+  var err error
   if clientset, err = common.InitClient(); err != nil {
     fmt.Println(err)
     return
   }
   http.HandleFunc("/ssh", wsHandler)
-  http.ListenAndServe(":7888", nil)
+  err = http.ListenAndServe(":7888", nil)
+  if err != nil {
+    return
+  }
 }
--- ws/connect.go
+++ ws/connect.go
@@ -8,123 +8,124 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var wsUpgrader = websocket.Upgrader {
-  CheckOrigin: func(r *http.Request) bool {
-    return true
-  },
+var wsUpgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 type WsMessage struct {
-  MessageType int
-  Data []byte 
+	MessageType int
+	Data        []byte
 }
 
 type WsConnection struct {
-  wsSocket *websocket.Conn
-  inChan chan *WsMessage
-  outChan chan *WsMessage
+	wsSocket *websocket.Conn
+	inChan   chan *WsMessage
+	outChan  chan *WsMessage
 
-  mutex sync.Mutex
-  isClosed bool
-  closeChan chan byte 
+	mutex     sync.Mutex
+	isClosed  bool
+	closeChan chan byte
 }
 
 func (wsConn *WsConnection) wsReadLoop() {
-  var (
-    msgType int
-    data []byte
-    msg *WsMessage
-    err error
-  )
-  for {
-    if msgType, data, err = wsConn.wsSocket.ReadMessage(); err != nil {
-      goto ERROR 
-    }
-    msg = &WsMessage{
-      msgType,
-      data,
-    }
-    select {
-    case wsConn.inChan <- msg:
-    case <- wsConn.closeChan:
-      goto CLOSED
-    }
-  }
+	var (
+		msgType int
+		data    []byte
+		msg     *WsMessage
+		err     error
+	)
+	for {
+		if msgType, data, err = wsConn.wsSocket.ReadMessage(); err != nil {
+			goto ERROR
+		}
+		msg = &WsMessage{
+			msgType,
+			data,
+		}
+		select {
+		case wsConn.inChan <- msg:
+		case <-wsConn.closeChan:
+			goto CLOSED
+		}
+	}
 ERROR:
-  wsConn.WsClose()
+	wsConn.WsClose()
 CLOSED:
 }
 
-
 func (wsConn *WsConnection) wsWriteLoop() {
-  var (
-    msg *WsMessage
-    err error 
-  )
-  for {
-    select {
-    case msg = <- wsConn.outChan:
-      if err = wsConn.wsSocket.WriteMessage(msg.MessageType, msg.Data); err != nil {
-        goto ERROR
-      }
-    case <- wsConn.closeChan:
-      goto CLOSED
-    }
-  }
+	var (
+		msg *WsMessage
+		err error
+	)
+	for {
+		select {
+		case msg = <-wsConn.outChan:
+			if err = wsConn.wsSocket.WriteMessage(msg.MessageType, msg.Data); err != nil {
+				goto ERROR
+			}
+		case <-wsConn.closeChan:
+			goto CLOSED
+		}
+	}
 ERROR:
-  wsConn.WsClose()
+	wsConn.WsClose()
 CLOSED:
 }
 
 func InitWebsocket(resp http.ResponseWriter, req *http.Request) (wsConn *WsConnection, err error) {
-  var (
-    wsSocket *websocket.Conn
-  )
-  if wsSocket, err = wsUpgrader.Upgrade(resp, req, nil); err != nil {
-    return
-  }
-  wsConn = &WsConnection{
-    wsSocket: wsSocket,
-    inChan: make(chan *WsMessage, 1000),
-    outChan: make(chan *WsMessage, 1000),
-    closeChan: make(chan byte),
-    isClosed: false,
-  }
+	var (
+		wsSocket *websocket.Conn
+	)
+	if wsSocket, err = wsUpgrader.Upgrade(resp, req, nil); err != nil {
+		return
+	}
+	wsConn = &WsConnection{
+		wsSocket:  wsSocket,
+		inChan:    make(chan *WsMessage, 1000),
+		outChan:   make(chan *WsMessage, 1000),
+		closeChan: make(chan byte),
+		isClosed:  false,
+	}
 
-  go wsConn.wsReadLoop()
-  go wsConn.wsWriteLoop()
+	go wsConn.wsReadLoop()
+	go wsConn.wsWriteLoop()
 
-  return
+	return
 }
 
 func (wsConn *WsConnection) WsWrite(messageType int, data []byte) (err error) {
-  select {
-  case wsConn.outChan <- &WsMessage{messageType, data}:
-  case <- wsConn.closeChan:
-    err = errors.New("websocker closed")
-  }
-  return
+	select {
+	case wsConn.outChan <- &WsMessage{messageType, data}:
+	case <-wsConn.closeChan:
+		err = errors.New("websocker closed")
+	}
+	return
 }
 
 func (wsConn *WsConnection) WsRead() (msg *WsMessage, err error) {
-  select {
-  case msg = <- wsConn.inChan:
-    return
-  case <- wsConn.closeChan:
-    err = errors.New("websocket closed")
-  }
-  return 
+	select {
+	case msg = <-wsConn.inChan:
+		return
+	case <-wsConn.closeChan:
+		err = errors.New("websocket closed")
+	}
+	return
 }
 
 func (wsConn *WsConnection) WsClose() {
-  wsConn.wsSocket.Close()
+	err := wsConn.wsSocket.Close()
+	if err != nil {
+		return
+	}
 
-  wsConn.mutex.Lock()
-  //defer wsConn.mutex.Unlock()
-  if !wsConn.isClosed {
-    wsConn.isClosed = true
-    close(wsConn.closeChan)
-  }
-  defer wsConn.mutex.Unlock()
+	wsConn.mutex.Lock()
+	//defer wsConn.mutex.Unlock()
+	if !wsConn.isClosed {
+		wsConn.isClosed = true
+		close(wsConn.closeChan)
+	}
+	defer wsConn.mutex.Unlock()
 }
-
